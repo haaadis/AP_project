e9a923340c698aadc3009e0d94bc24a805e3efe2
shahrzad <129215346+shziy@users.noreply.github.com>
2023-04-29 22:01:02 +0330

    Update acc.py
    raf yk sri bug

--- acc.py
+++ acc.py
@@ -1,21 +1,24 @@
 
-
 class Accounting(Order):
+    def __init__(self):
+        super().__init__()
     if (os.path.exists("accounting.xlsx") == False):
       w=Workbook()
       file=w.active
-      file.append(['order.no','quantity'])
+      file.append(['quantity','order.no','total price','send cost','tax'])
       file.freeze_panes = 'A2'
       w.save('accounting.xlsx')
     else:
         pass
 
-    def __init__(self):
-        super().__init__()
-    def hh(self):    
-          wb=load_workbook('accounting.xlsx')
-          ws = wb.active
-          ws.append([self.order_no,self.cart_item.loc['total']['quantity']])
-          wb.save('accounting.xlsx')
-          return open("accounting.xlsx","r+").read() 
+    def hh(self):
+        self.order_n0() 
+        cart=pd.read_csv('cart_item.csv')
+        wb=load_workbook('accounting.xlsx')
+        ws = wb.active
+        with open('conf.txt')as f:
+            lines=f.readlines()
+        ws.append(cart['quantity'].iloc[-1] , re.findall('\d+',lines[3]), cart['total price'].iloc[-1])
+        wb.save('accounting.xlsx')
+        return open("accounting.xlsx","r+").read() 
          
