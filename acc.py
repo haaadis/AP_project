d6d7e15f6aa9fe5dae6e4f3e32d1e480c04b1ffd
shahrzad <129215346+shziy@users.noreply.github.com>
2023-05-01 22:14:43 +0330

    Update acc.py
    edit kardan system hesab dari va raf yek seri bug ha

--- acc.py
+++ acc.py
@@ -1,24 +1,24 @@
 
-class Accounting(Order):
-    def __init__(self):
-        super().__init__()
+class Accounting:
     if (os.path.exists("accounting.xlsx") == False):
       w=Workbook()
       file=w.active
-      file.append(['quantity','order.no','total price','send cost','tax'])
+      file.append(['order','quantity','total price','send cost','tax'])
       file.freeze_panes = 'A2'
       w.save('accounting.xlsx')
     else:
         pass
 
-    def hh(self):
-        self.order_n0() 
-        cart=pd.read_csv('cart_item.csv')
-        wb=load_workbook('accounting.xlsx')
-        ws = wb.active
-        with open('conf.txt')as f:
-            lines=f.readlines()
-        ws.append(cart['quantity'].iloc[-1] , re.findall('\d+',lines[3]), cart['total price'].iloc[-1])
-        wb.save('accounting.xlsx')
-        return open("accounting.xlsx","r+").read() 
+    def hh(self,a,b,c):    
+          wb=load_workbook('accounting.xlsx')
+          ws = wb.active
+          p=[b,a.loc['total','quantity'],
+                    a.loc['total','total price'],
+                    (25 if c=='post' else 50)
+                    ,a.loc['total','total price']*0.09]
+          ws.append(p)
+          wb.save('accounting.xlsx')
+    def out(self):
+          df = pd.read_excel("accounting.xlsx")
+          return df 
          
