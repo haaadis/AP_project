db5cc8747452828850a076286021853104c3d9a7
siraliebi <127699992+siraliebi@users.noreply.github.com>
2023-05-01 23:59:50 +0330

    Update anbardary.py

--- anbardary.py
+++ anbardary.py
@@ -1,66 +1,58 @@
-
-
-
-
 class Inventory:
     #def update_admin_manual(self):
     # if mojodo --> manual 
     
     def stock_manual(self):
         while True:
-            in1=input(" bags or shoes").capitalize()
-            if in1=='Bags':
-                anbardari = pd.read_csv('C:/Users/ASUS/Downloads/Bags.csv')
-            else:
-                anbardari = pd.read_csv('C:/Users/ASUS/Downloads/Shoes.csv')
+            m = input('bags or shoes: ').capitalize() #choose
+            df = pd.read_csv(m+'.csv')
             id = int(input('Enter stock ID: '))
             new_stock = int(input('Enter new stock count: '))
-            anbardari.loc[anbardari['id'] == id, 'stock'] = new_stock        
+            df.loc[df['id'] == id, 'stock'] = new_stock        
             t=input('do you wanna continue')
             if t=='yes':
                 continue
             else:
-                anbardari.to_csv('my_file.csv',index=False)
+                df.to_csv('my_file.csv',index=False)
                 break
     def stock_csv(self):
        while True:
-            in1=input(" bags or shoes").capitalize()
-            if in1=='Bags':
-                anbardari = pd.read_csv('C:/Users/ASUS/Downloads/Bags.csv')
-            else:
-                anbardari = pd.read_csv('C:/Users/ASUS/Downloads/Shoes.csv')
+            m = input('bags or shoes: ').capitalize() #choose
+            df = pd.read_csv(m+'.csv')
             file=input()
             file=pd.read_csv(file)
-            anbardari.set_index('id', inplace=True)
-            anbardari.update(file.set_index('id'))
-            anbardari.reset_index()
+            df.set_index('id', inplace=True)
+            df.update(file.set_index('id'))
+            df.reset_index()
             t=input('do you wanna continue')
             if t=='yes':
                 continue
             else:
-                anbardari.to_csv('my_file.csv',index=False)
+                df.to_csv('my_file.csv',index=False)
                 break
         
     def add_item(self):
         while True:
-                in1=input(" bags or shoes").capitalize()
-                if in1=='Bags':
-                    anbardari = pd.read_csv('C:/Users/ASUS/Downloads/Bags.csv')
-                else:
-                    anbardari = pd.read_csv('C:/Users/ASUS/Downloads/Shoes.csv')
+                m = input('bags or shoes: ').capitalize() #choose
+                df = pd.read_csv(m+'.csv')
                 kala_w=input('enter w')
-                
                 kala_id=input('enter the id you wanna add')
                 kala_name=input('enter the name ')
                 kala_price=input('enter the unit price')
                 kala_color=input('enter color')
                 kala_stock=input('enter ths stock')
                 
-                anbardari.loc[len(anbardari.index)]=[kala_id,kala_name,kala_price,kala_stock,kala_color,kala_w]
+                df.loc[len(df.index)]=[kala_id,kala_name,kala_price,kala_stock,kala_color,kala_w]
                 t=input('do you wanna continue')
                 if t=='yes':
                     continue
                 
                 else:
-                    anbardari.to_csv('my_file.csv',index=False)
+                    df.to_csv('my_file.csv',index=False)
                     break
+    def update(self,m,id,quantity,color):
+        wh = m.items
+        res = wh.loc[(wh['id'] == id) & (wh['color'] == color), 'stock'].iloc[0]
+        wh.loc[(wh['id'] == id) & (wh['color'] == color), 'stock'] = res-quantity
+        m = m.replace(0, 'unavailable')
+        wh.to_csv(m.__name__+'.csv',index = False)
