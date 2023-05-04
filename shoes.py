cf6dfb2246482ce26390eefda267855aa5b8ab66
shahrzad <129215346+shziy@users.noreply.github.com>
2023-05-04 19:21:47 +0330

    Update shoes.py

--- shoes.py
+++ shoes.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 22 09:57:41 2023
-
-@author: Shayan
-"""
-
 import pandas as pd
 items = pd.DataFrame([{'id':1 ,'name': 'sandals', 'unit price':100 , 'stock':10,'color':{'brown','black'}},
         {'id':2 ,'name': 'sneakers', 'unit price':205 , 'stock':5,'color':{'white','blue'}},
@@ -13,3 +6,4 @@ items = pd.DataFrame([{'id':1 ,'name': 'sandals', 'unit price':100 , 'stock':10,
         {'id':5 ,'name': 'high hills sandals', 'unit price':850, 'stock':9,'color':{'white','black'}}])
 
 items= items.explode('color')
+items.to_csv('Bags.csv',index=False)
