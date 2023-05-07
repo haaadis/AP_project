c864a24824754fd1b23a33c9a10b90230921ac7f
shahrzad <129215346+shziy@users.noreply.github.com>
2023-05-07 20:41:30 +0330

    Update acc.py

--- acc.py
+++ acc.py
@@ -4,20 +4,20 @@ import pandas as pd
 from openpyxl import Workbook
 class Accounting:
     if (os.path.exists("accounting.csv") == False):    
-      file=pd.DataFrame()
-      file[['order','quantity','total price','send cost','tax']] = None
-      file.to_csv('accounting.csv')
+      file=pd.DataFrame(columns =['order','quantity','total price','send cost','tax'])
+      file.to_csv('accounting.csv',index=False)
     else:
         pass
 
     def acountant(self,a,b,c):    
           wb=pd.read_csv("accounting.csv")
-          wb.drop(['Unnamed: 0'], axis=1)
-          wb._append([b,a.loc['total','quantity'],
-                    a.loc['total','total price'],
-                    (25 if c=='post' else 50)
-                    ,a.loc['total','total price']*0.09])
-          wb.to_csv('accounting.csv')
+          wb=wb._append({'order' :b,'quantity' :a.loc['total','quantity'],
+
+                    'total price' :a.loc['total','total price'],
+                    'send cost':(25 if c=='post' else 50)
+                    ,'tax':a*0.09},ignore_index=True)
+          wb.to_csv('accounting.csv', index=False)
+
     def out(self):
           df = pd.read_csv("accounting.csv")
           return df 
